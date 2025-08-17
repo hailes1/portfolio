2bb38abad13507be6d7c205596b1d500db4e90c5
Dagmawe Amare Haileslassie <dag.haileslassie@gmail.com>
2025-08-16 21:03:48 -0500

    Portfolio-4: Setup Header, import carbon design system

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <template>
-  Hello, World!
+  <cv-header :style="headerStyle">
+    <template #header-global>
+      <cv-header-global-action aria-label="Switch" @click="onSwitch">
+        <Sun20 />
+      </cv-header-global-action>
+      <cv-header-global-action aria-label="Archive" @click="onSidebar">
+        <Archive20 />
+      </cv-header-global-action>
+    </template>
+  </cv-header>
 </template>
 
-<script setup>
+<script>
+import { CvHeader, CvHeaderName, CvHeaderNav, CvHeaderNavItem, CvHeaderGlobalAction } from '@carbon/vue';
+import { Sun20, Archive20 } from '@carbon/icons-vue';
 
+export default {
+  name: 'Header',
+  components: {
+    CvHeader,
+    CvHeaderName,
+    CvHeaderNav,
+    CvHeaderNavItem,
+    CvHeaderGlobalAction,
+    Archive20,
+    Sun20,
+  },
+  data(){
+    return {
+      isWhiteHeader: false,
+    };
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundColor: this.isWhiteHeader ? '#eef2f9' : '',
+      };
+    },
+  },
+  methods: {
+    onSwitch() {
+      this.isWhiteHeader = !this.isWhiteHeader;
+      console.log("Switch clicked");
+    },
+    onSidebar() {
+      console.log("Archive clicked");
+    },
+  },
+}
 </script>
-
-<style scoped>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import 'carbon-components/css/carbon-components.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'

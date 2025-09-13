8f12b637bb3f16d1ad8497f80f3f14f58425ff28
Dagmawe Amare Haileslassie <dag.haileslassie@gmail.com>
2025-09-13 11:14:48 -0500

    Portfolio-6: Update background color to accomodate the whole page

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,57 @@
 <template>
   <div>
-    <Header 
-      :class="[isLeftRailOpen ? 'left-rail-toggled' : '']" 
-      @update:left-rail-open="onLeftRailToggle" 
+    <Header
+      :class="[isLeftRailOpen ? 'left-rail-toggled' : '']"
+      @update:left-rail-open="onLeftRailToggle"
+      @update:switch-state="onSwitchToggle"
     />
-    <div class="left-rail" :class="{ open: isLeftRailOpen }">
-      <!-- <SidePanel /> -->
-    </div>
   </div>
 </template>
 
 <script>
-  import Header from './components/Header.vue';
+import Header from './components/Header.vue'
 
-  export default {
-    name: 'Main Page', 
-    components: {
-      Header,
-    },
-    data() {
-      return {
-        isLeftRailOpen: false,
-      };
-    },
-    methods: {
-      onLeftRailToggle(val) {
-        this.isLeftRailOpen = val;
-        console.log("Left Rail Toggled:", this.isLeftRailOpen);
-      }
+export default {
+  name: 'Main Page',
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      isLeftRailOpen: false,
+      isSwitchOn: false,
     }
-  }
+  },
+  methods: {
+    onLeftRailToggle(val) {
+      this.isLeftRailOpen = val
+      console.log('Left Rail Toggled:', this.isLeftRailOpen)
+    },
+    onSwitchToggle(val) {
+      this.isSwitchOn = val
+      if (val) {
+        document.body.classList.add('dark-mode')
+      } else {
+        document.body.classList.remove('dark-mode')
+      }
+      console.log('Switch Toggled:', this.isSwitchOn)
+    },
+  },
+}
 </script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  transition: background-color 0.3s ease;
+  background-color: #000000;
+}
+
+body.dark-mode {
+  background-color: #d3d3d3;
+}
+</style>
 
 <style scoped>
 .left-rail-toggled {
